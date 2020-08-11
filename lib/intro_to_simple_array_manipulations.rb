e5a8cbbb5f1772a4c4aafa24bbfc05384dbdab17
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-11 18:21:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, more_favs)
+  (my_favorite_things).concat(more_favs)
+end
+
+def using_insert(list_of_programming_languages, another_lanuage)
+  list_of_programming_languages.insert(4, another_lanuage)
+end
+
+def using_uniq(hair_cuts)
+  hair_cuts.uniq
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instructors, no_offense_steven)
+  instructors.delete(no_offense_steven)
+end
+
+def using_delete_at(famous_robots, delete_robot)
+  famous_robots.delete_at(2)
+end
